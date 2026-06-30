2da148a07e8a827aa561e41edbb0bbe388df85c4
SRIJAN SINGH <neetusrijan1212@gmail.com>
2026-06-30 17:01:35 +0530

    made some changes in UI and worked with useState

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -6,35 +6,58 @@ import './App.css'
 
 export default function App(){
 
-  function handleClickInc(){
-  setcount(count+1);
-}
-function handleClickDec(){
-  setcount(s=>{
-    if(s>0) return s-1;
-    return s;
-  }
-);
-}
-function handleClickRe(){
-  setcount(0);
-}
+  const [count, setCount] = useState(0);
 
-  const [count, setcount]=useState(0);
+  function handleClickInc(){
+    setCount(s => s + 1);
+  }
+
+  function handleClickFive(){
+    setCount(s => s + 5);
+  }
+
+  function handleClickDec(){
+    setCount(s => {
+      if(s > 0) return s - 1;
+      return s;
+    });
+  }
+
+  function handleClickRe(){
+    setCount(0);
+  }
 
   return(
-    <>
-    <h1>Countert_APP</h1>
-    <h1>{count}</h1>
-    <button onClick={handleClickInc}>
-      INCREASE
-    </button>
-    <button onClick={handleClickDec}>
-      DECREASE
-    </button>
-    <button onClick={handleClickRe}>
-      RESET
-    </button>
-    </>
+    <div className="container">
+      <h1>Countert_APP</h1>
+
+      <h1
+        className="counter"
+        style={{
+          color: count > 0 ? "green" : "gray"
+        }}
+      >
+        {count}
+      </h1>
+
+      {count === 0 && <p>No negative values allowed!</p>}
+
+      <button onClick={handleClickInc}>
+        INCREASE
+      </button>
+
+      <button onClick={handleClickDec} disabled={count === 0}>
+        DECREASE
+      </button>
+
+      <button onClick={handleClickFive}>
+        STEPUP+5
+      </button>
+
+      <button onClick={handleClickRe}>
+        RESET
+      </button>
+
+    </div>
   );
 }
